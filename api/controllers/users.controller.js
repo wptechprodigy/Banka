d54a51d77fecd853edec48fe0fa2a796e30bc4e4
Waheed Afolabi <waheed.freelancer16@gmail.com>
2019-04-07 21:57:20 +0100

    feature(users-route): implement user routes add dummyDB in utils directory add user model for creating users add user service CRUD operations add user controller to perform response operations add /Get all users route in index.js [Finishes #165164192]

---
+++ api/controllers/users.controller.js
@@ -0,0 +1,21 @@
+import UserService from '../services/users.service';
+
+const UserController = {
+  getAllUsers(req, res) {
+    const allUsers = UserService.findAllUsers();
+    res.status(200).json({
+      status: 200,
+      data: allUsers,
+    });
+  },
+  // getOneUserById(req, res) {
+  //     const id = req.params.id;
+  //     const foundUser = UserService.findOneUserById(id);
+  //     res.status(201).json({
+  //         status: 201,
+  //         data: foundUser,
+  //     });
+  // },
+};
+
+export default UserController;
---
+++ api/models/users.model.js
@@ -0,0 +1,16 @@
+class User {
+  constructor() {
+    this.id = null;
+    this.firstName = null;
+    this.lastName = null;
+    this.email = null;
+    this.password = null;
+    this.type = null;
+    this.phoneNumber = null;
+    this.address = null;
+    this.createdOn = null;
+    this.isAdmin = null;
+  }
+}
+
+export default User;
